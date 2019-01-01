87a0775e2bb45a6725663f0e2837c3bf575a8311
qinhuansky <chuanqin.yeh@foxmail.com>
2019-01-01 13:42:47 +0800

    add file

---
+++ housekeeping-vue/src/components/qinpages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>{{msg}}</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      msg: "Hello World!"
+    };
+  },
+  created() {}
+};
+</script>
+
+<style scope>
+</style>
